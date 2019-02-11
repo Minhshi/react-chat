e523f4aac7a3b5ca41b726bbb330ac18883f4030
Minh Pham <mpham@naver.com>
2019-02-11 16:06:14 +0100

    Add emojis and colored authors functionality

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -1,4 +1,17 @@
 import React from "react";
+import {emojify} from 'react-emojione';
+
+function strToRGB(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    console.log(hash)
+  }
+  const c = (hash & 0x00FFFFFF)
+    .toString(16)
+    .toUpperCase();
+  return `#${"00000".substring(0, 6 - c.length)}${c}`;
+}
 
 class Message extends React.Component {
   render() {
@@ -6,9 +19,9 @@ class Message extends React.Component {
     return (
       <div className="message-container">
         <div className="message-author">
-          <span>{this.props.message.author}</span> - {time}
+          <span style={{ color: strToRGB(this.props.message.author)}}>{this.props.message.author}</span> - {time}
         </div>
-        <div className="message-content">{this.props.message.content}</div>
+        <div className="message-content">{emojify(this.props.message.content)}</div>
       </div>
     );
   }
